7bb5133ca98e06034ecfe1e0f33e1660fe94e03f
wally <shinsanghoon@aivelabs.com>
2024-05-16 15:31:33 +0900

    test: 사용자 테스트 코드 작성

--- test/test_main.py
+++ test/test_main.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# src 디렉토리를 모듈 검색 경로에 추가
+sys.path.append(os.path.join(os.path.dirname(__file__), "../src"))
+
 from fastapi.testclient import TestClient
 import pytest
 from src.main import app
@@ -5,11 +11,8 @@ from src.main import app
 
 client = TestClient(app)
 
+
 @pytest.mark.describe("헬스체크에 성공하면 200을 리턴한다")
 def test_health_check():
-    response = client.get('/health')
+    response = client.get("/health")
     assert response.status_code == 200
-    
-
-    
-    
