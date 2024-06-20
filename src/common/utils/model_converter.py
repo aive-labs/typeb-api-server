027baf94911094e5c8e936fcfb44f117578ee1c0
wally <shinsanghoon@aivelabs.com>
2024-06-20 20:26:02 +0900

    feature: 모델 변환 유틸 함수 추가

--- src/common/utils/model_converter.py
+++ src/common/utils/model_converter.py
@@ -1,8 +1,13 @@
+from typing import Any, Dict, Type
+
+from pydantic import BaseModel, create_model
+
+
 class ModelConverter:
 
     @staticmethod
     def convert_entity_to_dict(entity):
-        print(entity)
+        """entity to dictionary"""
         return {
             column.name: getattr(entity, column.name)
             for column in entity.__table__.columns
@@ -10,5 +15,34 @@ class ModelConverter:
 
     @staticmethod
     def entity_to_model(entity, pydantic_model):
+        """entity to pydantic model"""
         entity_dict = ModelConverter.convert_entity_to_dict(entity)
         return pydantic_model(**entity_dict)
+
+    @staticmethod
+    def model_to_entity(pydantic_model: BaseModel, entity_class):
+        """pydantic model to entity"""
+        entity_dict = pydantic_model.model_dump()
+        return entity_class(**entity_dict)
+
+    @staticmethod
+    def model_to_model(
+        from_model: BaseModel, to_model_class: Type[BaseModel]
+    ) -> BaseModel:
+        fields = {
+            k: (v, ...)
+            for k, v in to_model_class.__annotations__.items()
+            if k in from_model.__dict__
+        }
+        to_model = create_model(
+            to_model_class.__name__, **fields
+        )  # pyright: ignore [reportArgumentType, reportCallIssue]
+        return to_model(**from_model.model_dump())
+
+    @staticmethod
+    def model_to_dict(model: BaseModel):
+        return model.model_dump()
+
+    @staticmethod
+    def dict_to_model(model_class: Type[BaseModel], data: Dict[str, Any]) -> BaseModel:
+        return model_class(**data)
