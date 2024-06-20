a1751bded4c1bb0ba16d3b009d49f24bcd48f5bc
wally <shinsanghoon@aivelabs.com>
2024-06-20 17:25:24 +0900

    feature: 문자, 카카오 발신자 번호 동일 여부 체크 로직 추가

--- src/auth/service/onboarding_service.py
+++ src/auth/service/onboarding_service.py
@@ -16,12 +16,27 @@ class OnboardingService(BaseOnboardingService):
     def register_message_sender(
         self, mall_id: str, message_sender: MessageSenderRequest
     ):
+        # 카카오 채널 정보가 있는 경우, 카카오에 등록된 sender_phone_number랑 같아야함
+        kakao_channel = self.onboarding_repository.get_kakao_channel(mall_id)
+        if kakao_channel:
+            if kakao_channel.sender_phone_number != message_sender.sender_phone_number:
+                raise Exception(
+                    "카카오 채널에 등록된 발신 번호와 등록하려는 발신 번호는 동일해야 합니다."
+                )
+
         self.onboarding_repository.save_message_sender(mall_id, message_sender)
 
     def get_message_sender(self, mall_id: str) -> MessageSenderResponse | None:
         return self.onboarding_repository.get_message_sender(mall_id)
 
     def register_kakao_channel(self, mall_id: str, kakao_channel: KakaoChannelRequest):
+        # 등록된 문자 발신자 정보가 있는 경우, 문자에 등록된 sender_phone_number랑 같아야함
+        message_sender = self.onboarding_repository.get_message_sender(mall_id)
+        if message_sender:
+            if message_sender.sender_phone_number != kakao_channel.sender_phone_number:
+                raise Exception(
+                    "등록된 문자 발신번호와 카카오 채널 발신 번호는 동일해야 합니다."
+                )
         self.onboarding_repository.save_kakao_channel(mall_id, kakao_channel)
 
     def get_kakao_channel(self, mall_id: str) -> KakaoChannelResponse | None:
