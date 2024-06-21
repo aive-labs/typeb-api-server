788730a9dc78dd7fb9ad8ff1d48d12acd0c94dc8
wally <shinsanghoon@aivelabs.com>
2024-06-21 11:21:14 +0900

    fix: global exception handler에 status code 추가

--- src/core/exceptions/exception_handlers.py
+++ src/core/exceptions/exception_handlers.py
@@ -6,9 +6,10 @@ from fastapi.responses import JSONResponse
 
 def global_exception_handler(request: Request, exc: HTTPException):
     return JSONResponse(
+        status_code=exc.status_code,
         content={
             "timestamp": datetime.now().isoformat() + "Z",
             "path": str(request.url),
             "detail": exc.detail,
-        }
+        },
     )
