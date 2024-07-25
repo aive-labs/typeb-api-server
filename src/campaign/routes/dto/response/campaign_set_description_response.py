97284efd640a457a7cb52572a9ad27deb6941f06
wally <shinsanghoon@aivelabs.com>
2024-07-25 13:18:07 +0900

    fix: 캠페인 정의서 필드명 수정

--- src/campaign/routes/dto/response/campaign_set_description_response.py
+++ src/campaign/routes/dto/response/campaign_set_description_response.py
@@ -6,16 +6,16 @@ from pydantic import BaseModel
 class CampaignSetDescription(BaseModel):
     set_seq: Optional[int] = None  # step2
     set_sort_num: Optional[int] = None
-    campaign_theme_id: Optional[int] = None
-    campaign_theme_name: Optional[str] = None
+    strategy_theme_id: Optional[int] = None
+    strategy_theme_name: Optional[str] = None
     audience_id: str
     audience_name: str
     audience_count: int
     audience_portion: float
     audience_unit_price: float
     response_rate: float
-    offer_id: Optional[str] = None
-    offer_name: Optional[str] = None
+    coupon_no: Optional[str] = None
+    coupon_name: Optional[str] = None
     recipient_count: int
     medias: Optional[str] = None
     contents_names: Optional[list[Union[str, None]]] = None  # 수정 로그(9)
