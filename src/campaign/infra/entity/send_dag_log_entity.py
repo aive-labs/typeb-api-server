7313aad1c4c875bfa7769c17e6c0651f19cfc6b3
wally <shinsanghoon@aivelabs.com>
2024-08-21 15:49:55 +0900

    fix: send_dag_log etl_time 디폴트값 추가

--- src/campaign/infra/entity/send_dag_log_entity.py
+++ src/campaign/infra/entity/send_dag_log_entity.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, DateTime, String
+from sqlalchemy import Column, DateTime, String, func
 
 from src.core.database import Base as Base
 
@@ -9,4 +9,4 @@ class SendDagLogEntity(Base):
     campaign_id = Column(String(20), nullable=False)
     send_resv_date = Column(DateTime(timezone=True), nullable=False)
     dag_run_id = Column(String, nullable=False, primary_key=True)
-    etl_time = Column(DateTime(timezone=True))
+    etl_time = Column(DateTime(timezone=True), default=func.now())
