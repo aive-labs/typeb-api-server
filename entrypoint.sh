15ad2b06d2a24b46e2a66fb230e0a87ea46ce3cf
wally <shinsanghoon@aivelabs.com>
2024-09-05 15:28:39 +0900

    settings: main 브랜치용 github action 스크립트 작성

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # AWS 설정
-aws configure set aws_access_key_id "$DEV_AWS_ACCESS_KEY_ID"
-aws configure set aws_secret_access_key "$DEV_AWS_SECRET_ACCESS_KEY"
-aws configure set default.region "$DEV_AWS_REGION"
+aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
+aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
+aws configure set default.region "$AWS_REGION"
 
 # 애플리케이션 시작
 exec "$@"
