f9461707898df0409cc529728e464b02700d6f8b
wally <shinsanghoon@aivelabs.com>
2024-11-11 17:19:40 +0900

    fix: alembic 테이블 존재하는 경우 패스하도록 수정
    - 테이블이 존재하는 경우 에러가 발생

--- migration/versions/20241107_1305_a8cb1cade643_add_personal_information_agree_status.py
+++ migration/versions/20241107_1305_a8cb1cade643_add_personal_information_agree_status.py
@@ -10,6 +10,7 @@ from typing import Sequence, Union
 
 import sqlalchemy as sa
 from alembic import op
+from sqlalchemy.engine import reflection
 
 # revision identifiers, used by Alembic.
 revision: str = "a8cb1cade643"
@@ -19,16 +20,23 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.create_table(
-        "outsourcing_personal_information_status",
-        sa.Column("term_status", sa.String(), nullable=False),
-        sa.Column("created_at", sa.DateTime(timezone=True), nullable=True),
-        sa.Column("created_by", sa.String(), nullable=False),
-        sa.Column("updated_at", sa.DateTime(timezone=True), nullable=True),
-        sa.Column("updated_by", sa.String(), nullable=False),
-        sa.PrimaryKeyConstraint("term_status"),
-        schema="aivelabs_sv",
-    )
+    conn = op.get_bind()
+    inspector = reflection.Inspector.from_engine(conn)
+
+    # 테이블 존재 여부 확인
+    if "outsourcing_personal_information_status" in inspector.get_table_names(schema="aivelabs_sv"):
+        print("Table 'outsourcing_personal_information_status' already exists. Skipping creation.")
+    else:
+        op.create_table(
+            "outsourcing_personal_information_status",
+            sa.Column("term_status", sa.String(), nullable=False),
+            sa.Column("created_at", sa.DateTime(timezone=True), nullable=True),
+            sa.Column("created_by", sa.String(), nullable=False),
+            sa.Column("updated_at", sa.DateTime(timezone=True), nullable=True),
+            sa.Column("updated_by", sa.String(), nullable=False),
+            sa.PrimaryKeyConstraint("term_status"),
+            schema="aivelabs_sv",
+        )
 
 
 def downgrade() -> None:
--- migration/versions/20241111_1026_ceca92cbab88_synchronize_dahsboard_table.py
+++ migration/versions/20241111_1026_ceca92cbab88_synchronize_dahsboard_table.py
@@ -10,6 +10,7 @@ from typing import Sequence, Union
 
 import sqlalchemy as sa
 from alembic import op
+from sqlalchemy.engine import reflection
 
 # revision identifiers, used by Alembic.
 revision: str = "ceca92cbab88"
@@ -19,117 +20,137 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade():
-    op.create_table(
-        "send_message_logs",
-        sa.Column("refkey", sa.String(100), nullable=False),
-        sa.Column("http_status_code", sa.Integer, nullable=False),
-        sa.Column("ppurio_messagekey", sa.String(2000), nullable=True),
-        sa.Column("http_request_body", sa.String(), nullable=True),
-        sa.Column("ppurio_status_code", sa.Integer, nullable=True),
-        sa.Column(
-            "created_at",
-            sa.TIMESTAMP(timezone=True),
-            server_default=sa.text("CURRENT_TIMESTAMP"),
-            nullable=True,
-        ),
-        sa.Column("send_resv_seq", sa.Integer, nullable=True),
-        sa.PrimaryKeyConstraint("refkey", "http_status_code", name="send_message_logs_pkey"),
-        schema="aivelabs_sv",
-    )
+    # 데이터베이스 연결 가져오기
+    conn = op.get_bind()
+    inspector = reflection.Inspector.from_engine(conn)
 
-    op.create_table(
-        "campaign_customer_snapshot",
-        sa.Column("cus_cd", sa.String, primary_key=True, nullable=False),
-        sa.Column("campaign_id", sa.String(10), primary_key=True, nullable=False),
-        sa.Column("campaign_name", sa.String, nullable=False),
-        sa.Column("send_date", sa.String, nullable=False),
-        sa.Column("start_date", sa.String, nullable=False),
-        sa.Column("end_date", sa.String, nullable=False),
-        sa.Column("campaign_status_name", sa.String, nullable=True),
-        sa.Column("campaign_group_id", sa.String, nullable=True),
-        sa.Column("strategy_id", sa.String, nullable=True),
-        sa.Column("strategy_name", sa.String, nullable=True),
-        sa.Column("group_sort_num", sa.Integer, nullable=True),
-        sa.Column("strategy_theme_id", sa.Integer, nullable=True),
-        sa.Column("strategy_theme_name", sa.String, nullable=True),
-        sa.Column("audience_id", sa.String, nullable=False),
-        sa.Column("audience_name", sa.String, nullable=False),
-        sa.Column("coupon_no", sa.String, nullable=True),
-        sa.Column("coupon_name", sa.String, nullable=True),
-        sa.Column("age_group_10", sa.String, nullable=True),
-        sa.Column("group_no", sa.BigInteger, nullable=True),
-        sa.Column("group_name", sa.String, nullable=True),
-        sa.Column("cv_lv2", sa.String, nullable=True),
-        sa.Column("recsys_model_id", sa.Integer, nullable=True),
-        sa.Column("recsys_model_name", sa.String, nullable=True),
-        sa.Column("etltime", sa.TIMESTAMP(timezone=True), nullable=True),
-        sa.PrimaryKeyConstraint("cus_cd", "campaign_id"),
-        schema="aivelabs_sv",
-    )
+    # 테이블 존재 여부 확인
+    if "send_message_logs" in inspector.get_table_names(schema="aivelabs_sv"):
+        print("Table 'send_message_logs' already exists. Skipping creation.")
+    else:
+        op.create_table(
+            "send_message_logs",
+            sa.Column("refkey", sa.String(100), nullable=False),
+            sa.Column("http_status_code", sa.Integer, nullable=False),
+            sa.Column("ppurio_messagekey", sa.String(2000), nullable=True),
+            sa.Column("http_request_body", sa.String(), nullable=True),
+            sa.Column("ppurio_status_code", sa.Integer, nullable=True),
+            sa.Column(
+                "created_at",
+                sa.TIMESTAMP(timezone=True),
+                server_default=sa.text("CURRENT_TIMESTAMP"),
+                nullable=True,
+            ),
+            sa.Column("send_resv_seq", sa.Integer, nullable=True),
+            sa.PrimaryKeyConstraint("refkey", "http_status_code", name="send_message_logs_pkey"),
+            schema="aivelabs_sv",
+        )
 
-    op.create_table(
-        "dash_daily_campaign_cost",
-        sa.Column("cus_cd", sa.String, nullable=False),
-        sa.Column("campaign_id", sa.String(10), nullable=False),
-        sa.Column("send_resv_date", sa.TIMESTAMP(timezone=True), nullable=True),
-        sa.Column("sent_success", sa.Text, nullable=True),
-        sa.Column("media", sa.Text, nullable=True),
-        sa.Column("cost_per_send", sa.Float, nullable=True),
-        sa.Column("etltime", sa.TIMESTAMP(timezone=True), nullable=True),
-        sa.PrimaryKeyConstraint("cus_cd", "campaign_id"),
-        schema="aivelabs_sv",
-    )
+    if "campaign_customer_snapshot" in inspector.get_table_names(schema="aivelabs_sv"):
+        print("Table 'campaign_customer_snapshot' already exists. Skipping creation.")
+    else:
+        op.create_table(
+            "campaign_customer_snapshot",
+            sa.Column("cus_cd", sa.String, primary_key=True, nullable=False),
+            sa.Column("campaign_id", sa.String(10), primary_key=True, nullable=False),
+            sa.Column("campaign_name", sa.String, nullable=False),
+            sa.Column("send_date", sa.String, nullable=False),
+            sa.Column("start_date", sa.String, nullable=False),
+            sa.Column("end_date", sa.String, nullable=False),
+            sa.Column("campaign_status_name", sa.String, nullable=True),
+            sa.Column("campaign_group_id", sa.String, nullable=True),
+            sa.Column("strategy_id", sa.String, nullable=True),
+            sa.Column("strategy_name", sa.String, nullable=True),
+            sa.Column("group_sort_num", sa.Integer, nullable=True),
+            sa.Column("strategy_theme_id", sa.Integer, nullable=True),
+            sa.Column("strategy_theme_name", sa.String, nullable=True),
+            sa.Column("audience_id", sa.String, nullable=False),
+            sa.Column("audience_name", sa.String, nullable=False),
+            sa.Column("coupon_no", sa.String, nullable=True),
+            sa.Column("coupon_name", sa.String, nullable=True),
+            sa.Column("age_group_10", sa.String, nullable=True),
+            sa.Column("group_no", sa.BigInteger, nullable=True),
+            sa.Column("group_name", sa.String, nullable=True),
+            sa.Column("cv_lv2", sa.String, nullable=True),
+            sa.Column("recsys_model_id", sa.Integer, nullable=True),
+            sa.Column("recsys_model_name", sa.String, nullable=True),
+            sa.Column("etltime", sa.TIMESTAMP(timezone=True), nullable=True),
+            sa.PrimaryKeyConstraint("cus_cd", "campaign_id"),
+            schema="aivelabs_sv",
+        )
 
-    op.create_table(
-        "dash_daily_date_ranges",
-        sa.Column("seq", sa.Text, nullable=True),
-        sa.Column("sale_dt", sa.Text, nullable=True),
-        sa.Column("campaign_id", sa.String(10), nullable=True),
-        sa.Column("campaign_name", sa.Text, nullable=True),
-        sa.Column("actual_start_date", sa.Text, nullable=True),
-        sa.Column("send_date", sa.Text, nullable=True),
-        sa.Column("start_date", sa.Text, nullable=True),
-        sa.Column("end_date", sa.Text, nullable=True),
-        sa.Column("campaign_status_name", sa.Text, nullable=True),
-        sa.Column("group_sort_num", sa.Integer, nullable=True),
-        sa.Column("strategy_theme_id", sa.Integer, nullable=True),
-        sa.Column("strategy_theme_name", sa.Text, nullable=True),
-        sa.Column("strategy_id", sa.String, nullable=True),
-        sa.Column("strategy_name", sa.Text, nullable=True),
-        sa.Column("audience_id", sa.String, nullable=True),
-        sa.Column("audience_name", sa.Text, nullable=True),
-        sa.Column("coupon_no", sa.String, nullable=True),
-        sa.Column("coupon_name", sa.Text, nullable=True),
-        sa.Column("media", sa.Text, nullable=True),
-        sa.Column("etltime", sa.TIMESTAMP(timezone=True), nullable=True),
-        sa.PrimaryKeyConstraint(
-            "campaign_id",
-            "audience_id",
-            "media",
-            "sale_dt",
-        ),
-        schema="aivelabs_sv",
-    )
+    if "dash_daily_campaign_cost" in inspector.get_table_names(schema="aivelabs_sv"):
+        print("Table 'dash_daily_campaign_cost' already exists. Skipping creation.")
+    else:
+        op.create_table(
+            "dash_daily_campaign_cost",
+            sa.Column("cus_cd", sa.String, nullable=False),
+            sa.Column("campaign_id", sa.String(10), nullable=False),
+            sa.Column("send_resv_date", sa.TIMESTAMP(timezone=True), nullable=True),
+            sa.Column("sent_success", sa.Text, nullable=True),
+            sa.Column("media", sa.Text, nullable=True),
+            sa.Column("cost_per_send", sa.Float, nullable=True),
+            sa.Column("etltime", sa.TIMESTAMP(timezone=True), nullable=True),
+            sa.PrimaryKeyConstraint("cus_cd", "campaign_id"),
+            schema="aivelabs_sv",
+        )
 
-    op.create_table(
-        "dash_daily_purchase_master",
-        sa.Column("cus_cd", sa.String, nullable=True),
-        sa.Column("sale_dt", sa.String, nullable=True),
-        sa.Column("recp_no", sa.String, nullable=True),
-        sa.Column("order_item_code", sa.String, nullable=True),
-        sa.Column("product_name", sa.String, nullable=True),
-        sa.Column("category_name", sa.String, nullable=True),
-        sa.Column("rep_nm", sa.String, nullable=True),
-        sa.Column("coupon_code", sa.String, nullable=True),
-        sa.Column("sale_amt", sa.Numeric, nullable=True),
-        sa.Column("sale_qty", sa.Numeric, nullable=True),
-        sa.Column("etltime", sa.TIMESTAMP(timezone=True), nullable=True),
-        sa.PrimaryKeyConstraint(
-            "cus_cd",
-            "order_item_code",
-        ),
-        schema="aivelabs_sv",
-    )
+    if "dash_daily_date_ranges" in inspector.get_table_names(schema="aivelabs_sv"):
+        print("Table 'dash_daily_date_ranges' already exists. Skipping creation.")
+    else:
+        op.create_table(
+            "dash_daily_date_ranges",
+            sa.Column("seq", sa.Text, nullable=True),
+            sa.Column("sale_dt", sa.Text, nullable=True),
+            sa.Column("campaign_id", sa.String(10), nullable=True),
+            sa.Column("campaign_name", sa.Text, nullable=True),
+            sa.Column("actual_start_date", sa.Text, nullable=True),
+            sa.Column("send_date", sa.Text, nullable=True),
+            sa.Column("start_date", sa.Text, nullable=True),
+            sa.Column("end_date", sa.Text, nullable=True),
+            sa.Column("campaign_status_name", sa.Text, nullable=True),
+            sa.Column("group_sort_num", sa.Integer, nullable=True),
+            sa.Column("strategy_theme_id", sa.Integer, nullable=True),
+            sa.Column("strategy_theme_name", sa.Text, nullable=True),
+            sa.Column("strategy_id", sa.String, nullable=True),
+            sa.Column("strategy_name", sa.Text, nullable=True),
+            sa.Column("audience_id", sa.String, nullable=True),
+            sa.Column("audience_name", sa.Text, nullable=True),
+            sa.Column("coupon_no", sa.String, nullable=True),
+            sa.Column("coupon_name", sa.Text, nullable=True),
+            sa.Column("media", sa.Text, nullable=True),
+            sa.Column("etltime", sa.TIMESTAMP(timezone=True), nullable=True),
+            sa.PrimaryKeyConstraint(
+                "campaign_id",
+                "audience_id",
+                "media",
+                "sale_dt",
+            ),
+            schema="aivelabs_sv",
+        )
+
+    if "dash_daily_purchase_master" in inspector.get_table_names(schema="aivelabs_sv"):
+        print("Table 'dash_daily_purchase_master' already exists. Skipping creation.")
+    else:
+        op.create_table(
+            "dash_daily_purchase_master",
+            sa.Column("cus_cd", sa.String, nullable=True),
+            sa.Column("sale_dt", sa.String, nullable=True),
+            sa.Column("recp_no", sa.String, nullable=True),
+            sa.Column("order_item_code", sa.String, nullable=True),
+            sa.Column("product_name", sa.String, nullable=True),
+            sa.Column("category_name", sa.String, nullable=True),
+            sa.Column("rep_nm", sa.String, nullable=True),
+            sa.Column("coupon_code", sa.String, nullable=True),
+            sa.Column("sale_amt", sa.Numeric, nullable=True),
+            sa.Column("sale_qty", sa.Numeric, nullable=True),
+            sa.Column("etltime", sa.TIMESTAMP(timezone=True), nullable=True),
+            sa.PrimaryKeyConstraint(
+                "cus_cd",
+                "order_item_code",
+            ),
+            schema="aivelabs_sv",
+        )
 
 
 def downgrade():
