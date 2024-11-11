b0248f95bb124970068f8fac9b86eb3105246bcd
wally <shinsanghoon@aivelabs.com>
2024-11-11 11:39:01 +0900

    fix: alembic 적용 명령어 도커 적용

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,9 @@ aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
 aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
 aws configure set default.region "$AWS_REGION"
 
+
+# alembic 버전 적용
+alembic upgrade head
+
 # 애플리케이션 시작
 exec "$@"
