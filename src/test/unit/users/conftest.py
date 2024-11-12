073e2581070d7d7af8280faa1b0135f9c28b410b
wally <shinsanghoon@aivelabs.com>
2024-11-12 18:17:00 +0900

    test: 사용자 정보 요청 API 테스트 추가

--- src/test/unit/users/conftest.py
+++ src/test/unit/users/conftest.py
@@ -1,6 +1,7 @@
 # test/users/conftest.py
 import pytest
 
+from src.test.unit.users.fixtures.create_token import create_access_token
 from src.test.unit.users.fixtures.mock_user_repository import get_mock_user_repository
 from src.users.service.user_service import UserService
 
@@ -13,3 +14,18 @@ def mock_user_repository():
 @pytest.fixture
 def user_service(mock_user_repository):
     return UserService(user_repository=mock_user_repository)
+
+
+@pytest.fixture
+def access_token():
+    payload = {
+        "email": "aivelabs",
+        "department": "aivelabs",
+        "mall_id": "aivelabs",
+        "language": "ko",
+        "permissions": "admin",
+        "role": "admin",
+        "subscription": {},
+    }
+    token = create_access_token(data=payload)
+    return token
