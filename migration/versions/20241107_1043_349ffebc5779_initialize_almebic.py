8db2ca26eacdf004fdb13b20ea2d2e94a74027ea
wally <shinsanghoon@aivelabs.com>
2024-11-21 15:43:29 +0900

    fix: subscription_plan 초기 데이터 추가

--- migration/versions/20241107_1043_349ffebc5779_initialize_almebic.py
+++ migration/versions/20241107_1043_349ffebc5779_initialize_almebic.py
@@ -1887,6 +1887,30 @@ def upgrade() -> None:
         unique=False,
         schema="aivelabs_sv",
     )
+
+    # 데이터 삽입
+    conn = op.get_bind()
+
+    exists = conn.execute(
+        text("SELECT 1 FROM aivelabs_sv.subscription_plans WHERE id = 1")
+    ).scalar()
+
+    if not exists:
+        op.execute(
+            """
+                INSERT INTO aivelabs_sv.subscription_plans (name, price, description, created_by, created_at, updated_by, updated_at)
+                VALUES (
+                    '1개월',
+                    30000,
+                    '한달 일별캠페인의 집행 금액 평균의 합계가 100만원 이하입니다.',
+                    '10',
+                    timezone('UTC', now()),
+                    '10',
+                    timezone('UTC', now())
+                );
+            """
+        )
+
     op.create_table(
         "user_passwords",
         sa.Column("login_id", sa.String(length=20), nullable=False),
